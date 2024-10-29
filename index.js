f84d3882158c62a616c833f765a2d861b80ac4fa
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-10-29 08:14:32 +0300

    Serve static directory & Add favicon to all views

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const express = require('express');
 const booksRouter = require('./routes/books-router.js');
-const authorsRouter = require('./routes/authors-router.js');
+const authorsGenresRouter = require('./routes/authors-genres-router.js');
+
+const VIEWS_DIR = path.join(process.cwd(), 'views');
+const PUBLIC_DIR = path.join(process.cwd(), 'public');
 
 const app = express();
 
@@ -9,12 +13,16 @@ const logger = (req, res, next) => {
   next();
 };
 
+app.set('views', VIEWS_DIR);
 app.set('view engine', 'ejs');
 
 app.use(logger);
+app.use(express.static(PUBLIC_DIR));
+
 app.all('/', (req, res) => res.redirect('/books'));
+
 app.use('/books', booksRouter);
-app.use('/authors', authorsRouter);
+app.use('(/authors|/genres)', authorsGenresRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
